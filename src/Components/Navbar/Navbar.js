328adb0182085d0b07b85696423b90e60859ba4b
Siddhartha <contact.sspanda@gmail.com>
2024-12-15 22:29:02 +0530

    Added the BusinessMotto Component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,8 +11,8 @@ const Navbar = () => {
         <div className='navbar-container bg-white py-1'>
             <div className='d-flex container justify-content-between'>
                 <div className='d-flex flex-column justify-content-center align-items-center'>
-                    <span style={{ fontWeight: '700', fontSize: 25, color: '#6700A3', lineHeight: '30px' }}>CODE</span>
-                    <span style={{ fontWeight: '500', fontSize: 16, color: '#6700A3', lineHeight: '20px' }}>UPLIFTER</span>
+                    <span className='logo-code'>CODE</span>
+                    <span className='logo-uplifter'>UPLIFTER</span>
                 </div>
                 <div className='navbar-menu-container'>
                     <span className='mx-3 navbar-menu'>Curriculum</span>
@@ -24,7 +24,7 @@ const Navbar = () => {
                     <span className='mx-3 navbar-menu'>FAQs</span>
                 </div>
                 <div className='navbar-mobile-menu' onClick={() => setShow(true)}>
-                    <MdMenu size={35} />
+                    <MdMenu size={25} />
                 </div>
             </div>
             <Modal
@@ -42,13 +42,13 @@ const Navbar = () => {
                         </div>
                         <div className='px-2 my-4'>
                             <div className='d-flex flex-column'>
-                                <span className='mx-3 navbar-menu my-3 pb-1 h3'>Curriculum</span>
-                                <span className='mx-3 navbar-menu my-3 pb-1 h3'>Reviews</span>
-                                <span className='mx-3 navbar-menu my-3 pb-1 h3'>Teaching</span>
-                                <span className='mx-3 navbar-menu my-3 pb-1 h3'>Mentorship</span>
-                                <span className='mx-3 navbar-menu my-3 pb-1 h3'>Community</span>
-                                <span className='mx-3 navbar-menu my-3 pb-1 h3'>Careers</span>
-                                <span className='mx-3 navbar-menu my-3 pb-1 h3'>FAQs</span>
+                                <span className='mx-3 navbar-menu my-2 pb-1'>Curriculum</span>
+                                <span className='mx-3 navbar-menu my-2 pb-1'>Reviews</span>
+                                <span className='mx-3 navbar-menu my-2 pb-1'>Teaching</span>
+                                <span className='mx-3 navbar-menu my-2 pb-1'>Mentorship</span>
+                                <span className='mx-3 navbar-menu my-2 pb-1'>Community</span>
+                                <span className='mx-3 navbar-menu my-2 pb-1'>Careers</span>
+                                <span className='mx-3 navbar-menu my-2 pb-1'>FAQs</span>
                             </div>
                         </div>
                     </div>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import FrontBanner from './../Components/FrontBanner/FrontBanner';
+import AcademyMotto from '../Components/AcamdeyMotto/AcademyMotto';
+import './HomePage.css'
 
 const HomePage = () => {
     return (
-        <div>
+        <div className='homepage-container'>
             <FrontBanner />
+            <AcademyMotto />
         </div>
     );
 }
